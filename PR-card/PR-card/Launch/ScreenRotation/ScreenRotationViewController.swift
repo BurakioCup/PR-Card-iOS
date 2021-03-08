27d90327cc383bba94ce96c288375e5ee5cb9ef9
KaitoKudou <kaitokudou2468110921@gmail.com>
2021-03-09 01:58:02 +0900

    [Add]暫定で３秒後に画面遷移するようにした

--- PR-card/PR-card/Launch/ScreenRotation/ScreenRotationViewController.swift
+++ PR-card/PR-card/Launch/ScreenRotation/ScreenRotationViewController.swift
@@ -9,21 +9,15 @@ import UIKit
 
 class ScreenRotationViewController: UIViewController {
 
+    var timer: Timer = Timer()
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        
+        timer = Timer.scheduledTimer(timeInterval: 3.0, target: self, selector: #selector(changeView), userInfo: nil, repeats: false)
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    @objc func changeView() {
+        print("３秒経過したので画面遷移")
     }
-    */
-
+    
 }
