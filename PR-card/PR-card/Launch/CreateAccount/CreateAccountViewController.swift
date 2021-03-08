b8c9943a72799e3763f8bb50da7a22274bcde7da
KaitoKudou <kaitokudou2468110921@gmail.com>
2021-03-08 23:59:58 +0900

    [Fix]push遷移に変更

--- PR-card/PR-card/Launch/CreateAccount/CreateAccountViewController.swift
+++ PR-card/PR-card/Launch/CreateAccount/CreateAccountViewController.swift
@@ -11,15 +11,17 @@ class CreateAccountViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        
+        view.backgroundColor = .white
+        self.navigationController?.navigationBar.barTintColor = #colorLiteral(red: 0.8781483173, green: 0.8781483173, blue: 0.8781483173, alpha: 1) // ナビゲーションバーの背景色
+        self.navigationController?.navigationBar.titleTextAttributes = [
+            // 文字の色
+            .foregroundColor: UIColor(hex: "9a9a9a", alpha: 1.0)
+            ]
     }
     
     @IBAction func createAccountButton(_ sender: Any) {
-        let storyboard = UIStoryboard(name: "MainTabBar", bundle: nil)
-        let mainTabBarVC = storyboard.instantiateViewController(identifier: "MainTabBar") as MainTabBarController
-        mainTabBarVC.modalPresentationStyle = .fullScreen
-        self.present(mainTabBarVC, animated: true, completion: nil)
-//        cardsVC.modalPresentationStyle = .fullScreen
-//        self.present(cardsVC, animated: true, completion: nil)
+        let storyboard = UIStoryboard(name: "SettingUserName", bundle: nil)
+        let settingUserNameVC = storyboard.instantiateViewController(identifier: "SettingUserName") as SettingUserNameViewController
+        navigationController?.pushViewController(settingUserNameVC, animated: true)
     }
 }
