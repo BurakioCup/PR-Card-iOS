cb5306358b1f2b9bb629f919c99de17ad4709832
KaitoKudou <kaitokudou2468110921@gmail.com>
2021-03-08 23:59:18 +0900

    [Fix]NavigationBarの色とテキストを変更

---
+++ PR-card/PR-card/Launch/ScreenRotation/ScreenRotationViewController.swift
@@ -0,0 +1,29 @@
+//
+//  ScreenRotationViewController.swift
+//  PR-card
+//
+//  Created by 工藤海斗 on 2021/03/08.
+//
+
+import UIKit
+
+class ScreenRotationViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
