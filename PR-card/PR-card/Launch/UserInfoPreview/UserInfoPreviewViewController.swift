4454bc6255a57f32e31fa7772ff1268daf163e43
KaitoKudou <kaitokudou2468110921@gmail.com>
2021-03-09 01:53:48 +0900

    [Add]パラメータ編集画面を作成

---
+++ PR-card/PR-card/Launch/UserInfoPreview/UserInfoPreviewViewController.swift
@@ -0,0 +1,29 @@
+//
+//  UserInfoPreviewViewController.swift
+//  PR-card
+//
+//  Created by 工藤海斗 on 2021/03/09.
+//
+
+import UIKit
+
+class UserInfoPreviewViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
