2b2864b967d0a9a0370fd510b70e2bfefd86de7b
KaitoKudou <kaitokudou2468110921@gmail.com>
2021-03-08 21:22:11 +0900

    [Add]初回起動のフラグを永続化

---
+++ PR-card/PR-card/Launch/CreateAccount/CreateAccountViewController.swift
@@ -0,0 +1,29 @@
+//
+//  CreateAccountViewController.swift
+//  PR-card
+//
+//  Created by 工藤海斗 on 2021/03/07.
+//
+
+import UIKit
+
+class CreateAccountViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
