5a9e1827f6727236ffe0a8088134f6ea2182f4ab
KaitoKudou <kaitokudou2468110921@gmail.com>
2021-03-09 01:56:21 +0900

    [Add]画面をデフォルトで横向きにした

--- PR-card/PR-card/Launch/ParametersEditPreview/ParametersEditPreviewViewController.swift
+++ PR-card/PR-card/Launch/ParametersEditPreview/ParametersEditPreviewViewController.swift
@@ -11,19 +11,33 @@ class ParametersEditPreviewViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        
+        //画面向きを左横画面でセットする
+        UIDevice.current.setValue(4, forKey: "orientation")
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    override var supportedInterfaceOrientations: UIInterfaceOrientationMask {
+        //左横画面に変更
+        if(UIDevice.current.orientation.rawValue == 4) {
+            UIDevice.current.setValue(4, forKey: "orientation")
+            return .landscapeLeft
+        } else {
+            //左横画面以外の処理
+            //右横画面に変更させる。
+            UIDevice.current.setValue(3, forKey: "orientation")
+            return .landscapeRight
+        }
     }
-    */
-
+    
+    // レーダーチャート再設定
+    @IBAction func reconfigurationButton(_ sender: Any) {
+        let storyboard = UIStoryboard(name: "ParametersEdit", bundle: nil)
+        let parametersEditVC = storyboard.instantiateViewController(identifier: "ParametersEdit") as ParametersEditViewController
+        navigationController?.pushViewController(parametersEditVC, animated: true)
+    }
+    
+    // レーダーチャート編集完了
+    @IBAction func finishButton(_ sender: Any) {
+    }
+    
 }
