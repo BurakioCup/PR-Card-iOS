55f82a9dfc0d5eadac7642ba6cce1a53b035451a
KaitoKudou <kaitokudou2468110921@gmail.com>
2021-03-09 00:01:01 +0900

    [Add]画面遷移を設定

--- PR-card/PR-card/Launch/SettingUserName/SettingUserNameViewController.swift
+++ PR-card/PR-card/Launch/SettingUserName/SettingUserNameViewController.swift
@@ -12,4 +12,10 @@ class SettingUserNameViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
     }
+    @IBAction func nameVoiceButton(_ sender: Any) {
+        print("voice")
+        let storyboard = UIStoryboard(name: "ScreenRotation", bundle: nil)
+        let screenRotationVC = storyboard.instantiateViewController(identifier: "ScreenRotation") as ScreenRotationViewController
+        navigationController?.pushViewController(screenRotationVC, animated: true)
+    }
 }
