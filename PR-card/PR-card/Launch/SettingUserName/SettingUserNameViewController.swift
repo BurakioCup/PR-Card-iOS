9a812081a7cbc3e2d470de85b3dd0948c961a409
KaitoKudou <kaitokudou2468110921@gmail.com>
2021-03-12 16:07:21 +0900

    [Add]マイクボタンを押したときにボタン画像を切り替える

--- PR-card/PR-card/Launch/SettingUserName/SettingUserNameViewController.swift
+++ PR-card/PR-card/Launch/SettingUserName/SettingUserNameViewController.swift
@@ -9,13 +9,30 @@ import UIKit
 
 class SettingUserNameViewController: UIViewController {
 
+    @IBOutlet weak var nameVoiceButtonImage: UIButton!
+    var isButtonPushed = false // 初期状態押されていない
+    var buttonImage: UIImage!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        buttonImage = UIImage(named: "Microphone")
+        nameVoiceButtonImage.setImage(buttonImage, for: .normal)
     }
+    
     @IBAction func nameVoiceButton(_ sender: Any) {
+        if isButtonPushed {
+            buttonImage = UIImage(named: "Microphone")
+            nameVoiceButtonImage.setImage(buttonImage, for: .normal)
+            isButtonPushed = false
+        } else {
+            buttonImage = UIImage(named: "Microphone_push")
+            nameVoiceButtonImage.setImage(buttonImage, for: .normal)
+            isButtonPushed = true
+        }
         print("voice")
-        let storyboard = UIStoryboard(name: "SettingUserPhoto", bundle: nil)
-        let settingUserPhotoVC = storyboard.instantiateViewController(identifier: "SettingUserPhoto") as SettingUserPhotoViewController
-        navigationController?.pushViewController(settingUserPhotoVC, animated: true)
+//        let storyboard = UIStoryboard(name: "SettingUserPhoto", bundle: nil)
+//        let settingUserPhotoVC = storyboard.instantiateViewController(identifier: "SettingUserPhoto") as SettingUserPhotoViewController
+//        navigationController?.pushViewController(settingUserPhotoVC, animated: true)
     }
 }
