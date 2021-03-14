ae97392b8c781f951ac4c9e57fdf6141f6348a6a
KaitoKudou <kaitokudou2468110921@gmail.com>
2021-03-15 02:25:57 +0900

    [Add]完了ボタンを追加

--- PR-card/PR-card/Launch/SettingUserName/SettingUserNameViewController.swift
+++ PR-card/PR-card/Launch/SettingUserName/SettingUserNameViewController.swift
@@ -6,59 +6,16 @@
 //
 
 import UIKit
-import Speech
-import AVFoundation
 
 class SettingUserNameViewController: UIViewController {
-
-    @IBOutlet weak var nameVoiceButtonImage: UIButton!
-    var isButtonPushed = false // 初期状態押されていない
-    var buttonImage: UIImage!
-    var recognizer = SFSpeechRecognizer(locale: Locale.init(identifier: "ja_JP"))!
-    var audioEngine: AVAudioEngine!
-    var recognitionReq: SFSpeechAudioBufferRecognitionRequest?
-    var recognitionTask: SFSpeechRecognitionTask?
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        
-        SFSpeechRecognizer.requestAuthorization({ authStatus in
-            OperationQueue.main.addOperation {
-                switch authStatus {
-                case .authorized:
-                    print("許可された")
-                
-                case .denied:
-                    print("拒否された")
-                
-                case .restricted: break
-                    
-                case .notDetermined: break
-                }
-            }
-        })
-        
-        buttonImage = UIImage(named: "Microphone")
-        nameVoiceButtonImage.setImage(buttonImage, for: .normal)
-        audioEngine = AVAudioEngine()
     }
     
-    @IBAction func nameVoiceButton(_ sender: Any) {
-        if isButtonPushed {
-            buttonImage = UIImage(named: "Microphone")
-            nameVoiceButtonImage.setImage(buttonImage, for: .normal)
-            isButtonPushed = false
-        } else {
-            buttonImage = UIImage(named: "Microphone_push")
-            nameVoiceButtonImage.setImage(buttonImage, for: .normal)
-            isButtonPushed = true
-        }
-        print("voice")
-//        let storyboard = UIStoryboard(name: "SettingUserPhoto", bundle: nil)
-//        let settingUserPhotoVC = storyboard.instantiateViewController(identifier: "SettingUserPhoto") as SettingUserPhotoViewController
-//        navigationController?.pushViewController(settingUserPhotoVC, animated: true)
-    }
-    
-    func startLiveTranscription() throws {
+    @IBAction func finish(_ sender: Any) {
+        let storyboard = UIStoryboard(name: "SettingUserPhoto", bundle: nil)
+        let settingUserPhotoVC = storyboard.instantiateViewController(identifier: "SettingUserPhoto") as SettingUserPhotoViewController
+        navigationController?.pushViewController(settingUserPhotoVC, animated: true)
     }
 }
