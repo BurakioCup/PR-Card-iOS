73692364c06102ad75686c9f82dd45e348d0a97e
KaitoKudou <kaitokudou2468110921@gmail.com>
2021-03-14 13:51:37 +0900

    [Add]音声認識の許可を設定

--- PR-card/PR-card/Launch/SettingUserName/SettingUserNameViewController.swift
+++ PR-card/PR-card/Launch/SettingUserName/SettingUserNameViewController.swift
@@ -6,18 +6,41 @@
 //
 
 import UIKit
+import Speech
+import AVFoundation
 
 class SettingUserNameViewController: UIViewController {
 
     @IBOutlet weak var nameVoiceButtonImage: UIButton!
     var isButtonPushed = false // 初期状態押されていない
     var buttonImage: UIImage!
+    var recognizer = SFSpeechRecognizer(locale: Locale.init(identifier: "ja_JP"))!
+    var audioEngine: AVAudioEngine!
+    var recognitionReq: SFSpeechAudioBufferRecognitionRequest?
+    var recognitionTask: SFSpeechRecognitionTask?
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        SFSpeechRecognizer.requestAuthorization({ authStatus in
+            OperationQueue.main.addOperation {
+                switch authStatus {
+                case .authorized:
+                    print("許可された")
+                
+                case .denied:
+                    print("拒否された")
+                
+                case .restricted: break
+                    
+                case .notDetermined: break
+                }
+            }
+        })
+        
         buttonImage = UIImage(named: "Microphone")
         nameVoiceButtonImage.setImage(buttonImage, for: .normal)
+        audioEngine = AVAudioEngine()
     }
     
     @IBAction func nameVoiceButton(_ sender: Any) {
@@ -34,5 +57,8 @@ class SettingUserNameViewController: UIViewController {
 //        let storyboard = UIStoryboard(name: "SettingUserPhoto", bundle: nil)
 //        let settingUserPhotoVC = storyboard.instantiateViewController(identifier: "SettingUserPhoto") as SettingUserPhotoViewController
 //        navigationController?.pushViewController(settingUserPhotoVC, animated: true)
+    }
+    
+    func startLiveTranscription() throws {
     }
 }
