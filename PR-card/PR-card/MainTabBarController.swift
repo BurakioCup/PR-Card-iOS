2b5e4a53d8ba106da82bcdb0cec1bf622a5e3cf8
KaitoKudou <kaitokudou2468110921@gmail.com>
2021-03-15 11:00:03 +0900

    [Add]viewをiPhone12に設定

--- PR-card/PR-card/MainTabBarController.swift
+++ PR-card/PR-card/MainTabBarController.swift
@@ -12,6 +12,22 @@ class MainTabBarController: UITabBarController {
     override func viewDidLoad() {
         super.viewDidLoad()
         selectedIndex = 1 // デフォルト画面を設定
+        
+        let userDefaults = UserDefaults.standard
+        let firstLunchKey = "firstLunchKey"
+        if userDefaults.bool(forKey: firstLunchKey) {
+            print("２回目以降の起動")
+            let storyboard = UIStoryboard(name: "MainTabBar", bundle: nil)
+            let mainTabBarController = storyboard.instantiateViewController(identifier: "MainTabBar") as MainTabBarController
+            mainTabBarController.modalPresentationStyle = .fullScreen
+            present(mainTabBarController, animated: true, completion: nil)
+        } else {
+            print("初回起動")
+            let storyboard = UIStoryboard(name: "CreateAccount", bundle: nil)
+            let createAccountVC = storyboard.instantiateViewController(identifier: "CreateAccount") as CreateAccountViewController
+            createAccountVC.modalPresentationStyle = .fullScreen
+            present(createAccountVC, animated: true, completion: nil)
+        }
     }
     
 }
