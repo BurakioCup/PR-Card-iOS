7cb047cbf1b7d5e4777be4255265ed900d145510
Toriyama Hidetaka <mgslove11zekuta@gmail.com>
2021-03-15 20:15:19 +0900

    [add]collectionviewにImage,Label、遷移を追加

--- PR-card/PR-card/View/CardsView/CardsViewController.swift
+++ PR-card/PR-card/View/CardsView/CardsViewController.swift
@@ -5,14 +5,17 @@
 //  Created by 工藤海斗 on 2021/03/07.
 //
 
+import Foundation
 import UIKit
 
 var cell = UICollectionView()
+//サーバデプロイまでの仮値
+let userName = ["a","b","c","d","e","f","g","h","i","j","k"]
+let SaveURL = ["a","b","c","d","e","f","g","h","i","j","k"]
 
-class CardsViewController: UIViewController,UIGestureRecognizerDelegate,UIScrollViewDelegate,UITextFieldDelegate, UICollectionViewDelegate{
+class CardsViewController: UIViewController,UIGestureRecognizerDelegate{
 
     @IBOutlet weak var collectionView: UICollectionView!
-    
     @IBAction func MyPRcard(_ sender: Any) {
         let vc = UIStoryboard(name: "MyQR", bundle: nil).instantiateViewController(withIdentifier:"Next") as! MyQRViewController
         present(vc, animated: true, completion: nil)
@@ -54,11 +57,38 @@ class CardsViewController: UIViewController,UIGestureRecognizerDelegate,UIScroll
     
     extension CardsViewController: UICollectionViewDataSource{
         func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
-            return 10
+            return userName.count
         }
             
         func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
             let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "CardsViewCell", for: indexPath) as! CardsViewCell
+            
+            let url = URL(string: (SaveURL[indexPath.row]))!
+        
+            do {
+                let data = try Data(contentsOf: url)
+                cell.userIconImage.image = UIImage(data: data)
+            } catch let err {
+                //画像がない場合デフォルトURLセット
+                cell.userIconImage.image = UIImage(named: "sample")
+            }
+            
+            cell.userNameLabel.text = userName[indexPath.row]
+            
             return cell
+        }
+    }
+
+    extension CardsViewController: UICollectionViewDelegate {
+        func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
+            
+            //var UserID = IDnumbers[indexPath.row]
+            //UserDefaults.standard.set(UserID, forKey: "userID")
+            
+            let vc2 = UIStoryboard(name: "OtherDetailCards", bundle: nil).instantiateViewController(withIdentifier:"OtherdetailcardsViewController") as! OtherDetailCardsViewController
+            vc2.modalPresentationStyle = .fullScreen
+            
+            navigationController?.pushViewController(vc2, animated: true)
+        
         }
     }
