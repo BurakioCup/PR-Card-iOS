a4a850a2265d4d0b6889bc46ecc85bde3d85835a
KaitoKudou <kaitokudou2468110921@gmail.com>
2021-03-16 12:33:08 +0900

    [Add]画面遷移を設定

--- PR-card/PR-card/Launch/ParametersItemIntroduction/ParametersItemIntroductionViewController.swift
+++ PR-card/PR-card/Launch/ParametersItemIntroduction/ParametersItemIntroductionViewController.swift
@@ -9,21 +9,36 @@ import UIKit
 
 class ParametersItemIntroductionViewController: UIViewController {
 
+    var timer: Timer = Timer()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        
+        //画面向きを左横画面でセットする
+        UIDevice.current.setValue(4, forKey: "orientation")
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(true)
+        timer = Timer.scheduledTimer(timeInterval: 1.0, target: self, selector: #selector(toParameterItemSetting), userInfo: nil, repeats: false)
     }
-    */
-
+    
+    override var supportedInterfaceOrientations: UIInterfaceOrientationMask {
+        //左横画面に変更
+        if(UIDevice.current.orientation.rawValue == 4) {
+            UIDevice.current.setValue(4, forKey: "orientation")
+            return .landscapeLeft
+        } else {
+            //左横画面以外の処理
+            //右横画面に変更させる。
+            UIDevice.current.setValue(3, forKey: "orientation")
+            return .landscapeRight
+        }
+    }
+    
+    @objc func toParameterItemSetting() {
+        let storyboard = UIStoryboard(name: "ParametersItemSetting", bundle: nil)
+        let parametersItemSettingVC = storyboard.instantiateViewController(identifier: "ParametersItemSetting") as ParametersItemSettingViewController
+        navigationController?.pushViewController(parametersItemSettingVC, animated: true)
+    }
 }
