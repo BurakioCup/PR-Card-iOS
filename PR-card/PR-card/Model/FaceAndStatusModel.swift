291bd73549185f4981c6ede949e3e2e10c41e331
KaitoKudou <kaitokudou2468110921@gmail.com>
2021-03-17 07:10:07 +0900

    [Fix]ステータス画像を表示

---
+++ PR-card/PR-card/Model/FaceAndStatusModel.swift
@@ -0,0 +1,8 @@
+//
+//  FaceAndStatusModel.swift
+//  PR-card
+//
+//  Created by 工藤海斗 on 2021/03/17.
+//
+
+import Foundation
---
+++ PR-card/PR-card/Presenter/ParametersEditPresenter.swift
@@ -0,0 +1,8 @@
+//
+//  ParametersEditPresenter.swift
+//  PR-card
+//
+//  Created by 工藤海斗 on 2021/03/17.
+//
+
+import Foundation
