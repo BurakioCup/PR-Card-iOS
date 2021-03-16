4094f02161e2bb907ee7cda22fe26ddac04df978
Toriyama Hidetaka <mgslove11zekuta@gmail.com>
2021-03-17 07:37:41 +0900

    [add]自分のプロフィールを表示

--- PR-card/PR-card/View/MyProfileView/MyProfileViewController.swift
+++ PR-card/PR-card/View/MyProfileView/MyProfileViewController.swift
@@ -7,10 +7,93 @@
 
 import UIKit
 
-class MyProfileViewController: UIViewController {
+let myProfilePresenter = MyProfilePresenter()
 
+class MyProfileViewController: UIViewController {
+    
+    @IBOutlet weak var FaceImage: UIImageView!
+    @IBOutlet weak var NameImage: UIImageView!
+    @IBOutlet weak var FreeImage: UIImageView!
+    @IBOutlet weak var TagImage: UIImageView!
+    @IBOutlet weak var StatusImage: UIImageView!
+    
+    @IBAction func ReturnEdit(_ sender: UIButton) {
+        let storyboard = UIStoryboard(name: "ParametersEdit", bundle: nil)
+        let parametersEditVC = storyboard.instantiateViewController(identifier: "ParametersEdit") as ParametersEditViewController
+        parametersEditVC.hidesBottomBarWhenPushed = true
+        navigationController?.pushViewController(parametersEditVC, animated: true)
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        myProfilePresenter.application(completion: { [weak self] response in
+            
+            guard let self = self else { return }
+            
+            
+            guard let faceimage = response.faceImage else { return }
+            print("ああああああ",faceimage)
+            let url1 = URL(string: (faceimage))!
+            
+            do {
+                let data = try Data(contentsOf: url1)
+                self.FaceImage.image = UIImage(data: data)
+            } catch let err {
+                //画像がない場合デフォルトURLセット
+                self.FaceImage.image = UIImage(named: "sample")
+            }
+            
+            
+            guard let nameimage = response.nameImage else { return }
+            let url2 = URL(string: (nameimage))!
+            
+            do {
+                let data = try Data(contentsOf: url2)
+                self.NameImage.image = UIImage(data: data)
+            } catch let err {
+                //画像がない場合デフォルトURLセット
+                self.NameImage.image = UIImage(named: "sample")
+            }
+            
+            
+            guard let statusImage = response.statusImage else { return }
+            let url3 = URL(string: (statusImage))!
+            
+            do {
+                let data = try Data(contentsOf: url3)
+                self.StatusImage.image = UIImage(data: data)
+            } catch let err {
+                //画像がない場合デフォルトURLセット
+                self.StatusImage.image = UIImage(named: "sample")
+            }
+            
+            
+            guard let tagimage = response.tagImage else { return }
+            let url4 = URL(string: (tagimage))!
+            
+            do {
+                let data = try Data(contentsOf: url4)
+                self.TagImage.image = UIImage(data: data)
+            } catch let err {
+                //画像がない場合デフォルトURLセット
+                self.TagImage.image = UIImage(named: "sample")
+            }
+            
+            
+            guard let freeimage = response.freeImage else { return }
+            let url5 = URL(string: (freeimage))!
+            
+            do {
+                let data = try Data(contentsOf: url5)
+                self.FreeImage.image = UIImage(data: data)
+            } catch let err {
+                //画像がない場合デフォルトURLセット
+                self.FreeImage.image = UIImage(named: "sample")
+            }
+            
+        })
+        
     }
     
     override func viewDidAppear(_ animated: Bool) {
