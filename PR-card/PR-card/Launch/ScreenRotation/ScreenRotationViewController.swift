0e8ac0695a82fa0b1b9378760807c6d1ffb667a5
KaitoKudou <kaitokudou2468110921@gmail.com>
2021-03-16 12:33:41 +0900

    [Fix]画面遷移を設定

--- PR-card/PR-card/Launch/ScreenRotation/ScreenRotationViewController.swift
+++ PR-card/PR-card/Launch/ScreenRotation/ScreenRotationViewController.swift
@@ -13,11 +13,14 @@ class ScreenRotationViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        timer = Timer.scheduledTimer(timeInterval: 3.0, target: self, selector: #selector(changeView), userInfo: nil, repeats: false)
+        timer = Timer.scheduledTimer(timeInterval: 2.0, target: self, selector: #selector(changeView), userInfo: nil, repeats: false)
     }
     
     @objc func changeView() {
         print("３秒経過したので画面遷移")
+        let storyboard = UIStoryboard(name: "ParametersItemIntroduction", bundle: nil)
+        let parametersItemIntroductionVC = storyboard.instantiateViewController(identifier: "ParametersItemIntroduction") as ParametersItemIntroductionViewController
+        navigationController?.pushViewController(parametersItemIntroductionVC, animated: true)
     }
     
 }
