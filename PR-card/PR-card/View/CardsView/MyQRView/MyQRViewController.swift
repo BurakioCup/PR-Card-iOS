47b5af87369fa58a06c81b54981e6b17985f56d5
Toriyama Hidetaka <mgslove11zekuta@gmail.com>
2021-03-17 02:52:21 +0900

    [add]MyQR関連のModel,Presenter

--- PR-card/PR-card/View/CardsView/MyQRView/MyQRViewController.swift
+++ PR-card/PR-card/View/CardsView/MyQRView/MyQRViewController.swift
@@ -5,13 +5,31 @@
 //  Created by 工藤海斗 on 2021/03/07.
 //
 
+import Foundation
 import UIKit
 
-class MyQRViewController: UIViewController {
+let myQRpresenter = MyQRPresenter()
 
+class MyQRViewController: UIViewController {
+@IBOutlet weak var QRimageView: UIImageView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        myQRpresenter.application(completion: { [weak self] response in
+            
+            guard let self = self else { return }
+            let MyQRImage = response.qrImage
+            
+            guard let encodedImageData = MyQRImage else { return }
+            let imageData = NSData(base64Encoded: encodedImageData)
+            let image = UIImage(data: imageData as! Data)
+            
+            
+            self.QRimageView.image = image
+            
+            
+        })
         
     }
 }
