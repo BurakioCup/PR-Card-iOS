36d239fb74398cf2d91637f1d84065d5da769b5e
Kaito Kudou <kaitokudou2468110921@gmail.com>
2022-07-22 00:23:14 +0900

    [fix]キーボードの上げ下げを制御

--- PR-card/PR-card/Launch/CreateAccount/CreateAccountViewController.swift
+++ PR-card/PR-card/Launch/CreateAccount/CreateAccountViewController.swift
@@ -40,47 +40,28 @@ class CreateAccountViewController: UIViewController {
         guard let idTextField = idTextField else { return }
         guard let passwordTextField = passwordTextField else { return }
         
-        // キーボード、画面全体、textFieldのsizeを取得
-        let rect = (notification.userInfo?[UIResponder.keyboardFrameEndUserInfoKey] as? NSValue)?.cgRectValue
-        guard let keyboardHeight = rect?.size.height else { return }
-        let mainBoundsSize = UIScreen.main.bounds.size
-        let idTextFieldHeight = idTextField.frame.height
-        let passwordTextFieldHeight = passwordTextField.frame.height
-        
-        // ① テキストフィールドの底辺より10.0下のy座標を取得
-        let idFieldPositionY = idTextField.frame.origin.y + idTextFieldHeight + 10.0
-        let passwordFieldPositionY = passwordTextField.frame.origin.y + passwordTextFieldHeight + 10.0
-        
-        // ② キーボードの底辺のy座標を取得
-        let keyboardPositionY = mainBoundsSize.height - keyboardHeight
-        
-        // ③キーボードをずらす
         if idTextField.isFirstResponder {
-            print("firstTextField.isFirstResponder")
-            let duration: TimeInterval? =
-            notification.userInfo?[UIResponder.keyboardAnimationDurationUserInfoKey] as? Double
-            UIView.animate(withDuration: duration!) {
-                // viewをy座標方向にtransformする
-                self.view.transform = CGAffineTransform(translationX: 0, y: keyboardPositionY - idFieldPositionY)
+            if view.frame.origin.y == 0 {
+                if let keyboardRect = (notification.userInfo?[UIResponder.keyboardFrameEndUserInfoKey] as? NSValue)?.cgRectValue {
+                    view.frame.origin.y -= keyboardRect.height
+                }
             }
-        } else if passwordTextField.isFirstResponder {
-            print("secondTextField.isFirstResponder")
-            let duration: TimeInterval? =
-            notification.userInfo?[UIResponder.keyboardAnimationDurationUserInfoKey] as? Double
-            UIView.animate(withDuration: duration!) {
-                // viewをy座標方向にtransformする
-                self.view.transform = CGAffineTransform(translationX: 0, y: keyboardPositionY - passwordFieldPositionY)
+        }
+        
+        if passwordTextField.isFirstResponder {
+            if view.frame.origin.y == 0 {
+                if let keyboardRect = (notification.userInfo?[UIResponder.keyboardFrameEndUserInfoKey] as? NSValue)?.cgRectValue {
+                    view.frame.origin.y -= keyboardRect.height
+                }
             }
         }
     }
     
     // キーボードが閉じられた時にViewの高さを元に戻す
     @objc func keyboardWillHide(notification: NSNotification) {
-        guard let userInfo = notification.userInfo else { return }
-        let duration: Float = (userInfo[UIResponder.keyboardAnimationDurationUserInfoKey] as! NSNumber).floatValue
-        UIView.animate(withDuration: TimeInterval(duration), animations: { () -> Void in
-            self.view.transform = CGAffineTransform.identity
-        })
+        if view.frame.origin.y != 0 {
+            view.frame.origin.y = 0
+        }
     }
 }
 
